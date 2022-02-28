e02544eb49105dcdd15a31f38a59961580a87b23
rambabu <rsunkaramondee@gmail.com>
2022-02-28 14:28:12 +0530

    fierst commit

---
+++ SpringBootRestexample/src/main/java/com/mondee/MainLogicEx.java
@@ -0,0 +1,15 @@
+package com.mondee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //@component scan,@enableautoconfig
+public class MainLogicEx {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MainLogicEx.class, args);
+		
+	}
+
+}
