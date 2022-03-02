6d8af3e4ecc8feeb4338a6c0f0b2db06a32aee6f
rambabu <rsunkaramondee@gmail.com>
2022-03-02 20:29:41 +0530

    first commit

---
+++ EmployeeManagerRest/src/main/java/com/rest/repository/ManagerRepository.java
@@ -0,0 +1,11 @@
+package com.rest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rest.model.Manager;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, Integer> {
+
+}
